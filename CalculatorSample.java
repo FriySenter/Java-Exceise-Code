e45b0afaf8b70ea8e3ef30edc5e54e812b6bb04d
Eric Wu <32679545+FriySenter@users.noreply.github.com>
2018-09-17 23:33:23 -0500

    Update CalculatorSample.java

--- CalculatorSample.java
+++ CalculatorSample.java
@@ -59,13 +59,5 @@ public class main {
 		}
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		main m = new main();
-
-		m.getInput();
-		System.out.println(m.result);
-
-	}
 
 }
